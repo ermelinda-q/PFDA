4050db19f43623f779beeb86e07a9ecf577210b1
ermelinda-q <ermelinda.qejvani@gmail.com>
2024-11-11 13:03:12 +0000

    start of assignment 6

---
+++ mywork/week 6/lecture1_breakout5.py
@@ -0,0 +1,12 @@
+# PFDA - Lecture 1, breakout 5
+# Author: E. Qejvani
+
+# Task: On July 16, 1969, the huge, 363-feet tall Saturn V rocket launches on the Apollo 11 mission from Pad A, Launch Complex 39, Kennedy Space Center,
+# at 9:32 a.m. EDT. Write a Python program the computes (**from now**) the number of:
+# - Years
+# - Months
+# - Days
+# - Hours
+# - Minutes
+# - Seconds             since the launch.
+
