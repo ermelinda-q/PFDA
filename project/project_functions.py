727e8aea2b7be3a20cfc30aa479f8d956bbc2961
ermelinda-q <ermelinda.qejvani@gmail.com>
2024-12-30 23:28:52 +0000

    cleaning the dataset

--- project/project_functions.py
+++ project/project_functions.py
@@ -10,26 +10,34 @@ import pandas as pd
 def count_rows_with_empty_or_space_cells_detail(df):  
     # Create a Boolean DataFrame where cells equal ' ' or NaN are True
     df_boolean = (df == ' ') | (df.isnull())  # Checks for both empty space ' ' and NaN values
-    
+
     # Count the number of missing values (spaces or NaN) in each row
     row_counts = df_boolean.sum(axis=1)
-    
-    # Create a dictionary to store how many rows have 1, 2, ..., n missing cells
-    missing_counts = row_counts.value_counts().sort_index()
 
-    # Print the detailed count of rows with empty cells
+    # Create a dictionary to store how many rows have missing cells
+    missing_counts = row_counts.value_counts().sort_index()
+    # Variable to hold the rows with at least one empty cell
+    total_rows_with_no_value = 0
+    # Print the detailed count of rows with empty cells and add the total
     for count, rows in missing_counts.items():
         print(f"Rows with {count} empty cells: {rows}")
+        if count > 0:  # Only add rows with at least one empty cell
+            total_rows_with_no_value += rows
+    
+    print(f"Total rows with at least one empty values: {total_rows_with_no_value}")
 
-    return missing_counts
+    # Return the total rows with all values
+    return total_rows_with_no_value
+
+   
 
 # This function cleans/removes all the rows that contain missing data in the DataFrame.
 def remove_rows_with_missing_data(df):  
     # Create a Boolean DataFrame where cells equal ' ' or NaN are True
-    df_boolean = (df == ' ') | (df.isnull())  # Checks for both empty space ' ' and NaN values
+    df_to_clean = (df == ' ') | (df.isnull())  # Checks for both empty space ' ' and NaN values
     
     # Remove rows with any missing data (either ' ' or NaN)
-    df_cleaned = df[~df_boolean.any(axis=1)]  # Keep rows where no cells are missing
+    df_cleaned = df[~df_to_clean.any(axis=1)]  # Keep rows where no cells are missing
     
     return df_cleaned
 
